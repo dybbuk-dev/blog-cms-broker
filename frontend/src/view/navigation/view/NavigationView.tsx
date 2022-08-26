28a6ff9b3c2cdd059674f8a91e284f07b0b84611
dybbuk555 <dybbuk555@gmail.com>
2022-08-26 16:28:14 +0900

    add: base view structure for `navigation`

---
+++ frontend/src/view/navigation/view/NavigationView.tsx
@@ -0,0 +1,28 @@
+import Spinner from 'src/view/shared/Spinner';
+import { i18n } from 'src/i18n';
+import TextViewItem from 'src/view/shared/view/TextViewItem';
+
+function NavigationView(props) {
+  const renderView = () => {
+    const { record } = props;
+
+    return (
+      <div>
+        <TextViewItem
+          label={i18n('entities.navigation.fields.name')}
+          value={record.name}
+        />
+      </div>
+    );
+  };
+
+  const { record, loading } = props;
+
+  if (loading || !record) {
+    return <Spinner />;
+  }
+
+  return renderView();
+}
+
+export default NavigationView;
