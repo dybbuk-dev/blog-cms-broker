d21c4233f2fa8e3422924a9130e7b08d6ced1ba4
dybbuk555 <dybbuk555@gmail.com>
2022-09-14 11:18:32 +0200

    add: frontend for pages

---
+++ frontend/src/modules/page/pageSelectors.tsx
@@ -0,0 +1,69 @@
+import { createSelector } from 'reselect';
+import authSelectors from 'src/modules/auth/authSelectors';
+import PermissionChecker from 'src/modules/auth/permissionChecker';
+import Permissions from 'src/security/permissions';
+
+const selectPermissionToRead = createSelector(
+  [
+    authSelectors.selectCurrentTenant,
+    authSelectors.selectCurrentUser,
+  ],
+  (currentTenant, currentUser) =>
+    new PermissionChecker(currentTenant, currentUser).match(
+      Permissions.values.pageRead,
+    ),
+);
+
+const selectPermissionToEdit = createSelector(
+  [
+    authSelectors.selectCurrentTenant,
+    authSelectors.selectCurrentUser,
+  ],
+  (currentTenant, currentUser) =>
+    new PermissionChecker(currentTenant, currentUser).match(
+      Permissions.values.pageEdit,
+    ),
+);
+
+const selectPermissionToCreate = createSelector(
+  [
+    authSelectors.selectCurrentTenant,
+    authSelectors.selectCurrentUser,
+  ],
+  (currentTenant, currentUser) =>
+    new PermissionChecker(currentTenant, currentUser).match(
+      Permissions.values.pageCreate,
+    ),
+);
+
+const selectPermissionToImport = createSelector(
+  [
+    authSelectors.selectCurrentTenant,
+    authSelectors.selectCurrentUser,
+  ],
+  (currentTenant, currentUser) =>
+    new PermissionChecker(currentTenant, currentUser).match(
+      Permissions.values.pageImport,
+    ),
+);
+
+const selectPermissionToDestroy = createSelector(
+  [
+    authSelectors.selectCurrentTenant,
+    authSelectors.selectCurrentUser,
+  ],
+  (currentTenant, currentUser) =>
+    new PermissionChecker(currentTenant, currentUser).match(
+      Permissions.values.pageDestroy,
+    ),
+);
+
+const pageSelectors = {
+  selectPermissionToRead,
+  selectPermissionToEdit,
+  selectPermissionToCreate,
+  selectPermissionToDestroy,
+  selectPermissionToImport,
+};
+
+export default pageSelectors;
