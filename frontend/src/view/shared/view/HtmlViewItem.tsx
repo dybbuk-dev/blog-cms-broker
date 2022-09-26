b1928be37693c39299634676e143122b583422cf
dybbuk555 <dybbuk555@gmail.com>
2022-09-26 15:28:10 +0900

    chore: remove unnecessary package

--- frontend/src/view/shared/view/HtmlViewItem.tsx
+++ frontend/src/view/shared/view/HtmlViewItem.tsx
@@ -1,14 +1,11 @@
-import MDBox from 'src/mui/components/MDBox';
-// import { Parser as HtmlToReactParser } from 'html-to-react';
-import parse from 'html-react-parser';
 import { selectMuiSettings } from 'src/modules/mui/muiSelectors';
+import MDBox from 'src/mui/components/MDBox';
 import MDTypography from 'src/mui/components/MDTypography';
+import parse from 'html-react-parser';
 
 function HtmlViewItem(props) {
   const { value, label } = props;
   const { darkMode } = selectMuiSettings();
-  // const htmlToReactParser = new HtmlToReactParser();
-  // const reactElement = htmlToReactParser.parse(value);
   return (
     <MDBox pt={2} position="relative">
       <MDTypography
